4b0849afe6e87d623c0a3dc8bdce19a3f3884c9a
Sebastian Schulz <schulz.siwy@gmail.com>
2016-11-13 22:35:47 +0100

    dependencies & input

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ from tornado.wsgi import WSGIContainer
 from tornado.web import Application, FallbackHandler
 from tornado.websocket import WebSocketHandler
 from tornado.ioloop import IOLoop, PeriodicCallback
+from datetime import datetime
+import json
 import random
 import string
 
@@ -15,21 +17,16 @@ class WebSocket(WebSocketHandler):
         self.clients.append(self)
         print("User connected")
 
-    def on_message(self, message):
-        print('User sent: {}'.format(message))
-        for client in self.clients:
-            if client.__room == self.__room:
-                client.write_message('You sent: {}'.format(message))
+    def on_message(self, payload):
+        msg = json.loads(payload)
+        if msg['type'] == 'message':
+            for client in self.clients:
+                if client.__room == self.__room:
+                    client.write_message(payload)
 
     def on_close(self):
         self.clients.remove(self)
         print("User disconnected")
-
-
-def test_messages():
-    msg = ''.join(random.choice(string.ascii_lowercase) for i in range(5))
-    for client in WebSocket.clients:
-        client.write_message('Server message: {}'.format(msg))
 
 if __name__ == "__main__":
     container = WSGIContainer(app)
@@ -39,9 +36,4 @@ if __name__ == "__main__":
     ], debug=True)
     server.listen(5000)
     main_loop = IOLoop.instance()
-    event_test_messages = PeriodicCallback(
-        test_messages,
-        500,
-        io_loop=main_loop)
-    event_test_messages.start()
     main_loop.start()
